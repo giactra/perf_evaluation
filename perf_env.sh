f498456985c79973ddc2cb8225192b03f79ccfc7
Giacomo Travaglini <giacomo.travaglini@gmail.com>
2021-03-11 14:33:31 +0000

    scripts: Allow users to source scripts from other directories
    This is touching perf_env and perf_run scripts
    
    Signed-off-by: Giacomo Travaglini <giacomo.travaglini@gmail.com>

--- perf_env.sh
+++ perf_env.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-export PATH=${PWD}/linux/tools/perf:${PATH}
+PERF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+export PATH=${PERF_DIR}/linux/tools/perf:${PATH}
--- perf_run.sh
+++ perf_run.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-PERF_BIN=${PWD}/linux/tools/perf/perf
-OUTDIR=out/"$(date +"%d-%m-%Y")"
+PERF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+PERF_BIN=${PERF_DIR}/linux/tools/perf/perf
+OUTDIR=${PERF_DIR}/out/"$(date +"%d-%m-%Y")"
 OUTFILE=${OUTDIR}/perf.data
 
 mkdir -p ${OUTDIR}
@@ -9,5 +11,5 @@ mkdir -p ${OUTDIR}
 ${PERF_BIN} record -F 99 --call-graph dwarf -o ${OUTFILE} -- $*
 ${PERF_BIN} script -i ${OUTFILE} | c++filt > ${OUTDIR}/out.perf
 
-${PWD}/FlameGraph/stackcollapse-perf.pl ${OUTDIR}/out.perf > ${OUTDIR}/out.folded
-${PWD}/FlameGraph/flamegraph.pl ${OUTDIR}/out.folded > ${OUTDIR}/out.svg
+${PERF_DIR}/FlameGraph/stackcollapse-perf.pl ${OUTDIR}/out.perf > ${OUTDIR}/out.folded
+${PERF_DIR}/FlameGraph/flamegraph.pl ${OUTDIR}/out.folded > ${OUTDIR}/out.svg
